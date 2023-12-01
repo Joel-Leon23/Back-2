b09f6f172757ed9bda9eee0f3543aeb9663d1f4d
Joel Leon <joeleoncs@outlook.com>
2023-12-01 02:35:19 -0600

    Vid 8. Zuul fiters impl

---
+++ netflix-zuul/src/main/java/com/microservice/zuul/filters/TimePostFilter.java
@@ -0,0 +1,54 @@
+package com.microservice.zuul.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class TimePostFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(TimePostFilter.class);
+	
+	@Override
+	public boolean shouldFilter() {
+		// 
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// 
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		
+		log.info("Logging Post info");
+		
+		Long startTime = (Long) request.getAttribute("startTime");
+		Long finalTime = System.currentTimeMillis();
+		
+		Long execTime = finalTime - startTime;
+		
+		log.info(String.format("Exec time in s: %s s", execTime.doubleValue() / 1000.00));
+		log.info(String.format("Exec time in ms: %s ms", execTime.toString()));
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// 
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		// 
+		return 1;
+	}
+
+}
