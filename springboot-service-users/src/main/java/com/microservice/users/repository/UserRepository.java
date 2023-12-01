dc9981d96c3d814ca63f382227e3824ed5c3202f
Joel Leon <joeleoncs@outlook.com>
2023-12-01 17:18:11 -0600

    Vid 7. Custom RestResource Path URL

--- springboot-service-users/src/main/java/com/microservice/users/repository/UserRepository.java
+++ springboot-service-users/src/main/java/com/microservice/users/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.microservice.users.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.microservice.users.entities.User;
 
@@ -10,7 +12,8 @@ import com.microservice.users.entities.User;
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
-	public User findByUsername(String username);
+	@RestResource(path = "buscar-username")
+	public User findByUsername(@Param("nombre") String username);
 	
 	
 	public User findByUsernameAndEmail(String username, String email);
