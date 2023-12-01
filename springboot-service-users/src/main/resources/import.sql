aea1200536287c3df033fa0e903be299e1a3796b
Joel Leon <joeleoncs@outlook.com>
2023-12-01 17:08:11 -0600

    Vid 6. Test Users Crud

--- springboot-service-users/src/main/resources/import.sql
+++ springboot-service-users/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO Users(username, password, enabled, name, last_name, email)VALUES('john_doe', 'password123', true, 'John', 'Doe', 'john.doe@example.com');
 INSERT INTO Users(username, password, enabled, name, last_name, email)VALUES('jane_smith', 'secure_password', true, 'Jane', 'Smith', 'jane.smith@example.com');
+
+INSERT INTO roles(name) VALUES ('ROLE_USER');
+INSERT INTO roles(name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO users_to_roles(user_id, roles_id) VALUES(1,1);
+INSERT INTO users_to_roles(user_id, roles_id) VALUES(2,2);
+INSERT INTO users_to_roles(user_id, roles_id) VALUES(2,1);
