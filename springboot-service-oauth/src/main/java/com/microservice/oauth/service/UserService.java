d82f974150f182361811522dba11f30170621f25
Joel Leon <joeleoncs@outlook.com>
2023-12-01 19:12:05 -0600

    Vid 13. Create UserDetailsService

---
+++ springboot-service-oauth/src/main/java/com/microservice/oauth/service/UserService.java
@@ -0,0 +1,46 @@
+package com.microservice.oauth.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.microservice.commons.users.entity.User;
+import com.microservice.oauth.client.UserFeignClient;
+
+
+public class UserService implements UserDetailsService {
+
+	private Logger log = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient client;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = client.findByUsername(username);
+		
+		if(user == null) {
+			log.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found: " + username);
+		}
+		
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(r -> new SimpleGrantedAuthority(r.getName()))
+				.peek(authority -> log.info("Role:" + authority.getAuthority()))
+				.collect(Collectors.toList());
+		
+		log.info("Authenticated User: " + username);
+		
+		
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getEnabled(), true, true, true, authorities);
+	}
+
+}
